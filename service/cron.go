9d3372f70b5496c7a6b686fb3682b538326f911c
Johnny O'Donnell <jodonnell2@blueorigin.com>
2025-08-11 11:20:18 -0700

    Make HTTP request to check service health

--- service/cron.go
+++ service/cron.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
+    "io"
+    "net/http"
+    "os"
     "time"
 )
 
 
+func runCheck(service string) bool {
+    resp, err := http.Get(service + "/health")
+    if err != nil {
+        fmt.Println("HTTP request error:", err)
+        return false
+    }
+    defer resp.Body.Close()
+
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println("Read body error:", err)
+        return false
+    }
+
+    var result bool
+    if err := json.Unmarshal(body, &result); err != nil {
+        fmt.Println("JSON unmarshal error:", err)
+        return false
+    }
+
+    return result
+}
+
 func checkService() {
-    fmt.Println("Checking service")
+    service := os.Getenv("SERVICE")
+    if service == "" {
+        service = "http://localhost:8080"
+    }
+
+    result := runCheck(service)
+
+    if result {
+        fmt.Println(fmt.Sprintf("Service %s is up", service))
+    } else {
+        fmt.Println(fmt.Sprintf("Unable to connect to %s", service))
+    }
 }
 
 func StartCron() {
