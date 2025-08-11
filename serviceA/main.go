e1bf8b3642334873e32921b579df61e2ffec9c8b
Johnny O'Donnell <jodonnell2@blueorigin.com>
2025-08-11 10:50:59 -0700

    Create basic web API server in Go

--- serviceA/main.go
+++ serviceA/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
 
+
+func isHealthy(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, true)
+}
 
 func main() {
     fmt.Println("ServiceA starting up...")
+
+    router := gin.Default()
+    router.GET("/health", isHealthy)
+
+    router.Run("localhost:8080")
 }
 
