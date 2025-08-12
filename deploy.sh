78afe7645bae1bd471372b49adc1d7880dd5d2bb
Johnny O'Donnell <jodonnell2@blueorigin.com>
2025-08-12 10:36:38 -0700

    Move deployment to yaml file

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,5 @@
 ./cleanup.sh
 
 # Deploy services
-kubectl create deployment demo-service-a --image=demo-service:current
-kubectl set env deployment/demo-service-a SERVICE=http://demo-service-b:8081
-kubectl expose deployment demo-service-a --type=ClusterIP --port=8080 --target-port=8080
-
-kubectl create deployment demo-service-b --image=demo-service:current
-kubectl set env deployment/demo-service-b SERVICE=http://demo-service-a:8080
-kubectl expose deployment demo-service-b --type=ClusterIP --port=8081 --target-port=8080
+kubectl apply -f kubernetes/services.yaml
 
